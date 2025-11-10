48760fb90659025c4e1ac503bf1e40f1180fbec2
Muhammad Ahmad Mubasher <bsef23a039@pucit.edu.pk>
2025-11-10 21:13:37 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-def main(): 
-print("Hello from Jenkins CI/CD with Python!") 
-if __name__ == "__main__": 
-main()
+def main():
+    # Add indentation here
+    print("Hello from Jenkins CI/CD with Python!")
+
+# Make sure you are calling the function
+if __name__ == "__main__":
+    main()
